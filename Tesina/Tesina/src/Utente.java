a945a0e6ba5c32275c4019e1d4df2f39f764ccac
GiovoEvah <64041002+GiovoEvah@users.noreply.github.com>
2022-11-26 17:21:11 +0100

    Add files via upload

---
+++ Tesina/Tesina/src/Utente.java
@@ -0,0 +1,112 @@
+
+public class Utente {
+	private String nome;
+	private String cognome;
+	private String codfisc;
+	private int matricola;
+	private String password;
+	private String email;
+	private double stipendio;
+	private double costoorario;
+	private String iban;
+	private int contmatricola = 0;
+	
+	public Utente() {
+		setNome(null);
+		setCognome(null);
+		setCodfisc(null);
+		setMatricola(0);
+		setPassword(null);
+		setEmail(null);
+		setStipendio(0);
+		setCostoorario(0);
+		setIban(null);
+	}
+	
+	public Utente(String nome, String cognome, String codfisc, int matricola, String password,
+			String email, String iban) {
+		setNome(nome);
+		setCognome(cognome);
+		setCodfisc(codfisc);
+		setMatricola(260000 + contmatricola);
+		setPassword(password);
+		setEmail(email);
+		setStipendio(0);
+		setCostoorario(costoorario);
+		setIban(iban);
+		contmatricola += 1;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	
+	public String getCodfisc() {
+		return codfisc;
+	}
+	public void setCodfisc(String codfisc) {
+		this.codfisc = codfisc;
+	}
+	
+	public int getMatricola() {
+		return matricola;
+	}
+	public void setMatricola(int matricola) {
+		this.matricola = matricola;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public double getStipendio() {
+		return stipendio;
+	}
+	public void setStipendio(double stipendio) {
+		this.stipendio = stipendio;
+	}
+	
+	public double getCostoorario() {
+		return costoorario;
+	}
+	public void setCostoorario(double costoorario) {
+		this.costoorario = costoorario;
+	}
+	
+	public String getIban() {
+		return iban;
+	}
+	public void setIban(String iban) {
+		this.iban = iban;
+	}
+
+	@Override
+	public String toString() {
+		return "Utente nome=" + nome + ", cognome=" + cognome + ", codfisc=" + codfisc + ", matricola=" + matricola
+				+ ", password=" + password + ", email=" + email + ", stipendio=" + stipendio + ", costoorario="
+				+ costoorario + ", iban=" + iban + "\n";
+	}
+		
+	/*porcatro*/
+	
+}
